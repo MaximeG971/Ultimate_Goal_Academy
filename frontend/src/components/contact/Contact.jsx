ccc6ea5778ddb70cf2a1c0a67b3c5a126cc46a3d
MAXIME GAEL <maximejordan.gael@gmail.com>
2024-01-17 18:21:48 +0100

    création des input pour le formulaire

--- frontend/src/components/contact/Contact.jsx
+++ frontend/src/components/contact/Contact.jsx
@@ -2,8 +2,27 @@ import React from "react";
 
 function Contact() {
   return (
-    <div>
-      <h1>Contact</h1>
+    <div className="infos-contact">
+      <h1>Contactez-nous !</h1>
+      <p>
+        N'hésitez pas à nous contacter pour avoir davantage d'informations
+        concernant nos différentes formules ou toute autre information
+        concernant l'académie.
+      </p>
+      <p>
+        Notre équipe s'engage à traiter votre demande dans un délai de 24h
+        maximum.
+      </p>
+      <div className="formulaire-container">
+        <input name="nom" type="text" placeholder="Votre nom" />
+        <input name="mail" type="text" placeholder="Votre adresse mail" />
+        <input
+          name="telephone"
+          type="text"
+          placeholder="Votre n° de téléphone (facultatif)"
+        />
+        <input name="message" type="text" placeholder="Votre message" />
+      </div>
     </div>
   );
 }
