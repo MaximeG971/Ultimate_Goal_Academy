07ae744b59f5e65a89ff57b087a3dba947e498cc
MAXIME GAEL <maximejordan.gael@gmail.com>
2024-01-17 18:03:54 +0100

    ajout de la dépendance react-responsive et ajustement positionnement navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,15 @@
+import { useMediaQuery } from "react-responsive";
 import { Outlet } from "react-router-dom";
 import Navbar from "./components/navbar/Navbar";
 import NavbarDesktop from "./components/navbarDesktop/NavbarDesktop";
 
 function App() {
+  const isDesktop = useMediaQuery({ minWidth: 700 });
+
   return (
     <div className="nav">
-      <NavbarDesktop />
-      <Navbar />
+      {isDesktop && <NavbarDesktop />}
+      {!isDesktop && <Navbar />}
       <Outlet />
     </div>
   );
