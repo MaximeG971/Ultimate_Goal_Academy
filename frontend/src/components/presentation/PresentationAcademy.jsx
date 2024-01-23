9ad45c61f6ef8b6b4b837a0770890285a0e34c0c
MAXIME GAEL <maximejordan.gael@gmail.com>
2024-01-23 12:07:29 +0100

    Rédaction des paragraphes de la section présentation

--- frontend/src/components/presentation/PresentationAcademy.jsx
+++ frontend/src/components/presentation/PresentationAcademy.jsx
@@ -1,7 +1,49 @@
 import React from "react";
 
 function PresentationAcademy() {
-  return <div>Qui sommes-nous ?</div>;
+  return (
+    <div>
+      <h2>ULTIMATE GOAL ACADEMY</h2>
+      <p>
+        Ultimate Goal Academy a été créée en 2023 par notre coach principal
+        Sidiki TRAORÉ. <br />
+        Les premières séances d'entraînements individuels et collectifs ont
+        débuté en août 2023 pour la saison footballistique 2023/2024.
+      </p>
+      <p>
+        Deux sections sont actuellement ouvertes: U9 et U15. <br />
+        Des séances par groupe resreint ou des séances individuelles sont
+        données aux joueurs évoluant dans des catégories différentes.
+      </p>
+      <p>
+        Notre académie compte aujourd'hui 42 membres âgés de 6 ans à 16 ans.
+      </p>
+      <p>
+        L'ouverture d'une section Baby Foot est prévue pour la saison 2024/2025.
+      </p>
+      <h2>NOS OBJECTIFS</h2>
+      <p>À travers ses séances, Ultimate Goal Academy a pour objectifs :</p>
+      <ul>
+        <li>Le perfectionnement technique</li>
+        <li>
+          Le développement de l'intelligence de jeu, prise d'informations,
+          anticipation
+        </li>
+        <li>Le travail sur la notion d'esprit d'équipe</li>
+        <li>L'amélioration des qualités individuelles du joueur</li>
+        <li>La gestion des émotions et le solutionnement des problématiques</li>
+      </ul>
+      <p>
+        Nos coachs ont pour principale mission de guider et d'aider les jeunes
+        joueurs à trouver leur style de jeu. <br />
+        Nos éducateurs les assistent dans l'amélioration de leurs qualités
+        innées et naturelles/
+      </p>
+      <p>
+        Notre académie accorde une grande importance à la pédagogie positive.
+      </p>
+    </div>
+  );
 }
 
 export default PresentationAcademy;
