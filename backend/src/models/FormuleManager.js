81ef666d949d275836f3f1bd0fe0a4d3ed0e6f7d
MAXIME GAEL <maximejordan.gael@gmail.com>
2024-01-31 10:26:31 +0100

    création du FormuleManager

---
+++ backend/src/models/FormuleManager.js
@@ -0,0 +1,70 @@
+const AbstractManager = require("./AbstractManager");
+
+class FormuleManager extends AbstractManager {
+  constructor() {
+    // Call the constructor of the parent class (AbstractManager)
+    // and pass the table name "formule" as configuration
+    super({ table: "formule" });
+  }
+
+  // The C of CRUD - Create operation
+
+  async create(formule) {
+    const { type, category } = formule;
+    // Execute the SQL INSERT query to add a new formule to the "formule" table
+    const [result] = await this.database.query(
+      `insert into ${this.table} (type, category) values (?, ?)`,
+      [type, category]
+    );
+
+    // Return the ID of the newly inserted formule
+    return result.insertId;
+  }
+
+  // The Rs of CRUD - Read operations
+
+  async read(id) {
+    // Execute the SQL SELECT query to retrieve a specific formule by its ID
+    const [rows] = await this.database.query(
+      `select * from ${this.table} where id = ?`,
+      [id]
+    );
+
+    // Return the first row of the result, which represents the formule
+    return rows[0];
+  }
+
+  async readAll() {
+    // Execute the SQL SELECT query to retrieve all formules from the "formule" table
+    const [rows] = await this.database.query(`select * from ${this.table}`);
+
+    // Return the array of formules
+    return rows;
+  }
+
+  // The U of CRUD - Update operation
+  // TODO: Implement the update operation to modify an existing formule
+
+  async update(formule, id) {
+    // Execute the SQL INSERT query to update the row with tie id on the "formule" table
+    const result = await this.database.query(
+      `update ${this.table} set ? where id = ?`,
+      [formule, id]
+    );
+
+    return result;
+  }
+
+  // The D of CRUD - Delete operation
+  // TODO: Implement the delete operation to remove an formule by its ID
+  async delete(id) {
+    const result = await this.database.query(
+      `delete from ${this.table} where id = ?`,
+      [id]
+    );
+
+    return result;
+  }
+}
+
+module.exports = FormuleManager;
