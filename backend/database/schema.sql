abd8ccdc56670d961767da1a1abb4811e433b065
MAXIME GAEL <maximejordan.gael@gmail.com>
2024-01-31 16:23:44 +0100

    ajout des insert dans la table formule

--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -8,12 +8,20 @@ create table coach (
 create table formule (
   id int unsigned primary key auto_increment not null,
   type VARCHAR(255) NOT NULL,
-  category VARCHAR(255) NOT NULL
+  description VARCHAR(255) NOT NULL,
+  coach_id INT UNSIGNED,
+  FOREIGN KEY (coach_id) REFERENCES coach(id)
 );
 
 insert into coach (name, speciality) VALUES ('Sidiki', 'Coach principal');
 insert into coach (name, speciality) VALUES ('Coco', 'Entraîneur U9 et spécifique gardiens');
 insert into coach (name, speciality) VALUES ('Ali', 'Référent Futsal');
+
+insert into formule (type, description, coach_id) VALUES ('Formule individuelle', 'Séances individualisées', 1);
+insert into formule (type, description, coach_id) VALUES ('Formule collective', "Séances collectives par petits groupes sous forme d'ateliers", 1);
+insert into formule (type, description, coach_id) VALUES ('Formule combinée', "Packaging séances individuelles et collectives", 1);
+
+
 
 
 
