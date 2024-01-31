35fd24967c7a9d6ad9b11b5c394270581256f840
MAXIME GAEL <maximejordan.gael@gmail.com>
2024-01-31 10:32:44 +0100

    mise en place du schema

--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -2,3 +2,17 @@ create table item (
   id int unsigned primary key auto_increment not null,
   title varchar(255) not null
 );
+
+create table coach (
+  id int unsigned primary key auto_increment not null,
+  name VARCHAR(255) NOT NULL,
+  speciality VARCHAR(255)
+);
+
+
+create table formule (
+  id int unsigned primary key auto_increment not null,
+  type VARCHAR(255) NOT NULL,
+  category VARCHAR(255) NOT NULL
+);
+
