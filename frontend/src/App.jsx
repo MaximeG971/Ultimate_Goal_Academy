d3622f08bbabfe0a3a8402a64a1a119b17f793e1
MAXIME GAEL <maximejordan.gael@gmail.com>
2024-02-01 11:21:46 +0100

    ajout du footer sur toutes les pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { useMediaQuery } from "react-responsive";
 import { Outlet } from "react-router-dom";
 import Navbar from "./components/navbar/Navbar";
 import NavbarDesktop from "./components/navbarDesktop/NavbarDesktop";
+import Footer from "./components/footer/Footer";
 
 function App() {
   const isDesktop = useMediaQuery({ minWidth: 700 });
@@ -10,6 +11,7 @@ function App() {
     <div className="nav">
       {isDesktop && <NavbarDesktop />}
       {!isDesktop && <Navbar />}
+      <Footer />
       <Outlet />
     </div>
   );
