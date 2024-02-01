af34c42e3fb5f659fda6e9dee046e5cf44733161
MAXIME GAEL <maximejordan.gael@gmail.com>
2024-02-01 17:01:00 +0100

    création bouton edit

---
+++ frontend/src/components/dashboard/EditButton.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./EditButton.css";
+
+function EditButton() {
+  return (
+    <button type="button" className="edit-button">
+      Modifier
+    </button>
+  );
+}
+
+export default EditButton;
