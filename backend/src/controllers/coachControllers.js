58469155da6e263b9e3f6861924026b1dba2e070
MAXIME GAEL <maximejordan.gael@gmail.com>
2024-02-01 16:01:18 +0100

    activation du delete

--- backend/src/controllers/coachControllers.js
+++ backend/src/controllers/coachControllers.js
@@ -71,19 +71,19 @@ const read = async (req, res, next) => {
 
 // The D of BREAD - Destroy (Delete) operation
 // This operation is not yet implemented
-// const destroy = async (req, res, next) => {
-//   // Extract the coach data from the request body
-//   try {
-//     // Insert the coach into the database
-//     await tables.coach.delete(req.params.id);
+const destroy = async (req, res, next) => {
+  // Extract the coach data from the request body
+  try {
+    // Insert the coach into the database
+    await tables.coach.delete(req.params.id);
 
-//     // Respond with HTTP 204 (No Content)
-//     res.sendStatus(204);
-//   } catch (err) {
-//     // Pass any errors to the error-handling middleware
-//     next(err);
-//   }
-// };
+    // Respond with HTTP 204 (No Content)
+    res.sendStatus(204);
+  } catch (err) {
+    // Pass any errors to the error-handling middleware
+    next(err);
+  }
+};
 
 // Ready to export the controller functions
 module.exports = {
@@ -91,5 +91,5 @@ module.exports = {
   read,
   // edit,
   // add,
-  // destroy,
+  destroy,
 };
