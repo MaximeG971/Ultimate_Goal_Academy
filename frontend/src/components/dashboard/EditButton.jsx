174ff023055896458f7c909eefeb5c595beb74f0
MAXIME GAEL <maximejordan.gael@gmail.com>
2024-02-02 10:17:28 +0100

    ajout de la logique sur le bouton

--- frontend/src/components/dashboard/EditButton.jsx
+++ frontend/src/components/dashboard/EditButton.jsx
@@ -1,12 +1,16 @@
-import React from "react";
+import PropTypes from "prop-types";
 import "./EditButton.css";
 
-function EditButton() {
+function EditButton({ onClick }) {
   return (
-    <button type="button" className="edit-button">
+    <button type="button" className="edit-button" onClick={onClick}>
       Modifier
     </button>
   );
 }
+
+EditButton.propTypes = {
+  onClick: PropTypes.func.isRequired,
+};
 
 export default EditButton;
