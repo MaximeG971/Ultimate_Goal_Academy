8d2a90bb2d6b1d61dd3f051685ed422521159050
MAXIME GAEL <maximejordan.gael@gmail.com>
2024-02-02 10:17:51 +0100

    ajout du formulaire put et post pour les formules

--- frontend/src/components/dashboard/DashboardFormule.jsx
+++ frontend/src/components/dashboard/DashboardFormule.jsx
@@ -5,6 +5,10 @@ import EditButton from "./EditButton";
 
 function DashboardFormule() {
   const [formuleData, setFormuleData] = useState([]);
+  const [formuleValue, setFormuleValue] = useState({
+    type: "",
+    description: "",
+  });
 
   const getFormules = async () => {
     try {
@@ -12,6 +16,34 @@ function DashboardFormule() {
       setFormuleData(res.data);
     } catch (error) {
       console.error(error);
+    }
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormuleValue((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = async () => {
+    try {
+      const response = await connexion.post(`/formules`, formuleValue);
+
+      if (response.status === 201) {
+        console.info("Données enregistrées !");
+      } else {
+        console.error(
+          "Erreur lors de la soumission des données :",
+          response.statusText
+        );
+      }
+    } catch (error) {
+      console.error(
+        "Erreur lors de la soumission des données: ",
+        error.message
+      );
     }
   };
 
@@ -27,9 +59,60 @@ function DashboardFormule() {
     }
   };
 
+  const loadFormule = (formuleToUpdate) => {
+    setFormuleValue(formuleToUpdate);
+  };
+
+  const putCoach = async () => {
+    try {
+      await connexion.put(`/coachs/${formuleValue.id}`, formuleValue);
+      getFormules();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleRequest = (event) => {
+    if (formuleValue.id) {
+      putCoach(event);
+    } else {
+      handleSubmit(event);
+    }
+  };
+
   return (
     <div className="dashboard-coach">
       <h1>Gestion des formules</h1>
+      <div className="formulaire-crud-formule">
+        <form onSubmit={handleRequest} className="form-formule">
+          <label>
+            Type de formule
+            <input
+              type="text"
+              name="name"
+              value={formuleValue.type}
+              onChange={handleChange}
+              required
+            />
+          </label>
+          <label>
+            Description
+            <input
+              type="text"
+              name="description"
+              value={formuleValue.description}
+              onChange={handleChange}
+              required
+            />
+          </label>
+
+          <div className="container-submit-formule">
+            <button type="submit" className="bouton-submit-formule">
+              {formuleValue.id ? "Modifier" : "Ajouter"}
+            </button>
+          </div>
+        </form>
+      </div>
       <table className="table">
         <thead>
           <tr className="title-table">
@@ -50,7 +133,7 @@ function DashboardFormule() {
                 <DeleteButton onClick={() => deleteData(el.id)} />
               </td>
               <td className="button-container" aria-label="delete-button">
-                <EditButton />
+                <EditButton onClick={() => loadFormule(el)} />
               </td>
             </tr>
           ))}
