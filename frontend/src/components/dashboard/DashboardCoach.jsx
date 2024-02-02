ea3c4452ddf563a28c05b0942e9964e730a95d2c
MAXIME GAEL <maximejordan.gael@gmail.com>
2024-02-02 10:16:56 +0100

    ajout du formulaire pour post et put

--- frontend/src/components/dashboard/DashboardCoach.jsx
+++ frontend/src/components/dashboard/DashboardCoach.jsx
@@ -7,6 +7,11 @@ import "./DashboardCoach.css";
 
 function DashboardCoach() {
   const [coachData, setCoachData] = useState([]);
+  const [coachValue, setCoachValue] = useState({
+    name: "",
+    speciality: "",
+    photo: "",
+  });
 
   const getCoaches = async () => {
     try {
@@ -14,6 +19,34 @@ function DashboardCoach() {
       setCoachData(res.data);
     } catch (error) {
       console.error(error);
+    }
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setCoachValue((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = async () => {
+    try {
+      const response = await connexion.post(`/coachs`, coachValue);
+
+      if (response.status === 201) {
+        console.info("Données enregistrées !");
+      } else {
+        console.error(
+          "Erreur lors de la soumission des données :",
+          response.statusText
+        );
+      }
+    } catch (error) {
+      console.error(
+        "Erreur lors de la soumission des données: ",
+        error.message
+      );
     }
   };
 
@@ -29,9 +62,68 @@ function DashboardCoach() {
     }
   };
 
+  const loadCoach = (coachToUpdate) => {
+    setCoachValue(coachToUpdate);
+  };
+
+  const putCoach = async () => {
+    try {
+      await connexion.put(`/coachs/${coachValue.id}`, coachValue);
+      getCoaches();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleRequest = (event) => {
+    if (coachValue.id) {
+      putCoach(event);
+    } else {
+      handleSubmit(event);
+    }
+  };
+
   return (
     <div className="dashboard-coach">
       <h1>Gestion des coachs</h1>
+      <div className="formulaire-crud-coach">
+        <form onSubmit={handleRequest} className="form-coach">
+          <label>
+            Nom du nouveau coach
+            <input
+              type="text"
+              name="name"
+              value={coachValue.name}
+              onChange={handleChange}
+              required
+            />
+          </label>
+          <label>
+            Spécialité
+            <input
+              type="text"
+              name="speciality"
+              value={coachValue.speciality}
+              onChange={handleChange}
+              required
+            />
+          </label>
+          <label>
+            Spécialité
+            <input
+              type="text"
+              name="photo"
+              value={coachValue.photo}
+              onChange={handleChange}
+            />
+          </label>
+          <div className="container-submit-coach">
+            <button type="submit" className="bouton-submit-coach">
+              {coachValue.id ? "Modifier" : "Ajouter"}
+            </button>
+          </div>
+        </form>
+      </div>
       <table className="table">
         <thead>
           <tr className="titre-table">
@@ -54,7 +146,7 @@ function DashboardCoach() {
                 <DeleteButton onClick={() => deleteData(el.id)} />
               </td>
               <td className="button-container" aria-label="delete-button">
-                <EditButton />
+                <EditButton onClick={() => loadCoach(el)} />
               </td>
             </tr>
           ))}
